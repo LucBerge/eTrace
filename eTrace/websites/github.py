a950116cc7468b05a8321b84f348d5f9be1f9fc8
Lucas <lucas.bergeron@outlook.fr>
2019-07-29 19:52:56 +0200

    Minor update

--- eTrace/websites/github.py
+++ eTrace/websites/github.py
@@ -5,11 +5,10 @@ EMAIL = "noreply@github.com"
 
 def ask(email):
 
-	s = requests.session()
 	URL = 'https://github.com/password_reset'
 
-	r1 = s.get(URL)
-	authenticity_token = re.search("""<input type="hidden" name="authenticity_token" value="(.*?)"[^>]*?>""",r1.text,re.S|re.I)
+	r1 = requests.get(URL)
+	authenticity_token = re.search("""<input type="hidden" name="authenticity_token" value="(.*?)"[^>]*?>""",r1.text)
 
 	payload = {
 	    'authenticity_token': authenticity_token.group(1),
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-#!/usr/bin/python
-
-import setuptools
+import sys
+if sys.version_info < (3, 5):
+    sys.exit('Y2T requires Python 3.5+')
 
 long_description = open("README.md", "r").read()
 
+import setuptools
 setuptools.setup(
 	name='eTrace',
 	version='1.0',
 	author='LucBerge',
 	author_email='lucas.bergeron@outlook.fr',
+	description="Trace your email on internet",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url='https://github.com/LucBerge/eTrace',
